bb79c5edf8a8473df3ca33a3be36cb0be46e136c
quangquan <leo.langlois2@gmail.com>
2021-12-17 15:37:36 +0100

    tous délete

--- src/metier/Client.java
+++ src/metier/Client.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Client
 {
-	private static List<Client> clients;
+	private static List<Client> clients = new ArrayList<Client>();
 	private String nom;
 	private List<Facture> facture = new ArrayList<Facture>();
 	/** 
